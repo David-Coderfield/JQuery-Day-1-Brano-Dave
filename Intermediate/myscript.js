7fc49b7621685617161c06d396c0f576102e0eba
Branislav Simic <branislavsmc@outlook.de>
2019-10-07 10:39:41 +0200

    intermediate done

--- Intermediate/myscript.js
+++ Intermediate/myscript.js
@@ -6,3 +6,25 @@ $("form").submit(function(e) {
 	
 
 });
+
+
+// remove all form from the page
+// $("form").remove(); 
+
+
+// Crate 5 new DIV's
+$("article").append("<div></div> <div></div> <div></div> <div></div> <div></div>");
+
+// add class name of 'box' to each new DIV
+$("article div").addClass('box');
+
+
+// Within the input field change the value to "Search for..."
+$("input:text").val('Search for...');
+
+
+// Change the link to "www.codefactory.wien"
+$("a").attr("href",'www.codefactory.wien');
+
+
+
